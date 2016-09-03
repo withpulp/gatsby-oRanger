239d82923954b1a5b286bd315d4a649dd8e7cfc8
Damir <damir@withpulp.com>
2016-09-02 22:02:39 -0400

    refactoring page nests wip

---
+++ constants/social.js
@@ -0,0 +1,15 @@
+const social = [{
+	name: 'Twitter',
+	link: 'http://twitter.com/'
+}, {
+	name: 'Github',
+	link: 'http://github.com/'
+}, {
+	name: 'Codepen',
+	link: 'http://codepen.com/'
+}, {
+	name: 'Instagram',
+	link: 'http://instagram.com/'
+}];
+
+export default social;
