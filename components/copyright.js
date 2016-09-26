9a3725d2b0af57f0419667e59fa18f1e79af5d3a
Damir <damir@withpulp.com>
2016-09-26 15:05:06 -0400

    more refactoring

---
+++ components/copyright.js
@@ -0,0 +1,23 @@
+import React, { Component, PropTypes } from 'react';
+import HeartIcon from 'react-icons/lib/fa/heart';
+import { rhythm, fontSizeToMS } from 'utils/typography';
+
+class Copyright extends Component {
+	render() {
+		const { meta } = this.props;
+
+		return (
+			<figure className="copyright figure">
+				<p className="message">
+          &copy; {meta.year}, <span dangerouslySetInnerHTML={{__html: meta.message}}/>
+        </p>
+			</figure>
+		);
+	}
+};
+
+Copyright.propTypes = {
+	meta: PropTypes.object
+};
+
+export default Copyright;
---
+++ containers/footer/index.js
@@ -0,0 +1,48 @@
+import React, { Component, PropTypes } from 'react';
+import moment from 'moment';
+import include from 'underscore.string/include';
+import find from 'lodash/find';
+import Next from 'containers/next';
+import Copyright from 'components/copyright';
+import { config } from 'config';
+
+import 'containers/footer/index.css';
+
+class Footer extends Component {
+  render() {
+    const { location, route } = this.props;
+    const copyright = {
+      year: moment().year(),
+      message: config.copyrightMessage
+    };
+
+    let footer;
+
+    if (include(location.pathname, '/blog/') || include(location.pathname, '/work/')) {
+      const page = find(route.pages, { 'path': location.pathname});
+
+      footer = (
+        <footer className="footer article">
+          <Next data={page.data} pages={route.pages}/>
+          <Copyright meta={copyright} />
+        </footer>
+      );
+    } else {
+      footer = (
+        <footer className="footer">
+          <h6 className="title">Version: {config.siteVersion}</h6>
+          <Copyright meta={copyright} />
+        </footer>
+      );
+    }
+
+    return footer;
+  }
+}
+
+Footer.propTypes = {
+  location: PropTypes.object,
+  route: PropTypes.object
+};
+
+export default Footer;
