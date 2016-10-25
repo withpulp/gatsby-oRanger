fbe624d74a0b7bd87ddcc63ce9f9abd3064bd1df
Damir Vazgird <damir@withpulp.com>
2016-10-25 11:45:38 -0400

    Update html.js

--- html.js
+++ html.js
@@ -24,7 +24,7 @@ class Html extends Component {
                     <meta charSet="utf-8"/>
                     <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
                     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-                    <meta name="description" content={config.bookDescription} />
+                    <meta name="description" content={config.siteDescription} />
                     <title>{title || DocumentTitle.rewind()}</title>
                     <TypographyStyle typography={typography}/>
                     <GoogleFont typography={typography}/>
