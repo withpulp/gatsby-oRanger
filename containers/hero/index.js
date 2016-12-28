ff4a24d3d6a2ff3bd17bd16992e6fe92be6a0b63
Damir Vazgird <damir@withpulp.com>
2016-12-27 18:29:05 -0800

    some fixes to hero container

--- containers/hero/index.js
+++ containers/hero/index.js
@@ -7,37 +7,6 @@ import { config } from 'config';
 import 'containers/hero/index.css';
 
 class Hero extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      scrolling: false
-    }
-  }
-
-  componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
-  }
-
-  handleScroll(event) {
-    // @TODO: reduce opacity of title to 0 as scrolling down to bottom of hero
-    // no jquery, try to do this with js only
-    let scrollTop = event.srcElement.body.scrollTop;
-
-    // console.log(scrollTop);
-    // console.log(itemTranslate);
-    // @TODO: need a way to select the title from react virtual dom
-    // and change it's opacity as scrollTop reaches a certain value (300, 400, etc)
-    // can use state or ref - which one is more effective?
-    // https://facebook.github.io/react/docs/more-about-refs.html
-    // https://medium.com/react-tutorials/react-state-14a6d4f736f5#.rzm8mx92f
-    // http://stackoverflow.com/questions/29725828/update-style-of-a-component-onscroll-in-react-js
-  }
-
   render() {
     const { meta, route } = this.props;
     const page = route.page;
