aaafd4f974fee09cb00c94c3954f1446c0ff25f4
Damir Vazgird <damir@withpulp.com>
2017-08-22 17:01:09 -0400

    footer shows quotes on more pages

--- src/containers/footer/index.jsx
+++ src/containers/footer/index.jsx
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import Link from 'gatsby-link';
 import Quotes from '../../components/quotes/';
@@ -6,19 +7,35 @@ import quotes from '../../../data/quotes';
 import './index.css';
 
 class Footer extends Component {
-  render() {
-    const location = this.props.location;
-    const url = config.siteRss;
+  renderCTA() {
+    const path = this.props.location.pathname;
 
+    let cta;
+
+    // @TODO: refactor to
+    // if path includes '/blog' show null
+    // else show quotes
+    // _.include(path, '/blog')
+    if (path === '/' || path === '/about' || path === '/404')  {
+      cta = (
+        <Quotes data={quotes} />
+      )
+    } else {
+      cta = null;
+    }
+
+    return cta;
+  }
+
+  render() {
+    const url = config.siteRss;
     // @TODO: create caption component for copyright message
     // add url to company/author website inside copyright message
 
     return (
       <footer className="footer">
         <section className="affixed bottom section">
-          {location.pathname === '/' ?
-            <Quotes data={quotes} />
-          : null }
+          { this.renderCTA() }
           <p className="copyright caption">{config.copyright}</p>
         </section>
       </footer>
