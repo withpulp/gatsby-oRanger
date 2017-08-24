81491707958ba9983ce01a3a1ccb67f40b27a3a3
Damir Vazgird <damir@withpulp.com>
2017-08-23 21:40:39 -0400

    quick fix to readnext comp

--- src/components/read_next/index.jsx
+++ src/components/read_next/index.jsx
@@ -6,14 +6,16 @@ class ReadNext extends React.Component {
   render() {
     const { previous, next } = this.props;
 
+    // @TODO: add posts into link href from current url or prop
+
     return (
       <figure className="read next figure">
         <Link className="link previous"
-              to={`/${previous}`}>
+              to={`/posts/${previous}`}>
           <small className="label">Read Previous</small> {previous.split('-').join(' ')}
         </Link>
         <Link className="link next"
-              to={`/${next}`}>
+              to={`/posts/${next}`}>
           <small className="label">Read Next</small> {next.split('-').join(' ')}
         </Link>
       </figure>
